b9a78e600cdc7e4614337418eeb195b849f3c361
Mauricio Lorenzetti <mauricio@legend-mbp.local>
2017-09-16 11:21:49 -0300

    results screen created

---
+++ SoccerTracking/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  SoccerTracking
+//
+//  Created by Mauricio Lorenzetti on 16/09/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SoccerTracking/ResultsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultsViewController.swift
+//  SoccerTracking
+//
+//  Created by Mauricio Lorenzetti on 16/09/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
